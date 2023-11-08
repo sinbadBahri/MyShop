1a6ca3577f762c46428bbd43fc163a0958051740
sinbadBahri <h.l.kh7990@gmail.com>
2023-11-08 17:11:39 +0330

    Editing multiple Models in the Django Admin Site

--- shop/apps/product/admin.py
+++ shop/apps/product/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Category, Product, Brand
+from .models import Category, Product, Brand, ProductLine
+
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
+
 
 admin.site.register(Category)
-admin.site.register(Product)
 admin.site.register(Brand)
