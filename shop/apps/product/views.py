accad403f7eeb6d3d6d1f35202a82c7e18d7881b
sinbadBahri <h.l.kh7990@gmail.com>
2023-11-08 16:45:08 +0330

    List of all products based on a category option added to ProductVieSet

--- shop/apps/product/views.py
+++ shop/apps/product/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 
@@ -30,4 +31,20 @@ class ProductViewSet(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+        url_name="all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__title=category),
+            many=True,
+        )
         return Response(serializer.data)
