2fd4035d961b8b61122406e686e44c515a8af53b
sinbadBahri <h.l.kh7990@gmail.com>
2023-11-08 14:18:35 +0330

    model ProductLine added

--- shop/apps/product/models.py
+++ shop/apps/product/models.py
@@ -36,3 +36,11 @@ class Product(models.Model):
 
     def __str__(self) -> CharField:
         return self.title
+
+
+class ProductLine(models.Model):
+    price = models.IntegerField()
+    sku = models.CharField(max_length=100)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="product_lines")
+    is_active = models.BooleanField(default=False)
