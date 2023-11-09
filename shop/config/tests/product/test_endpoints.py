d9bfe6be40cf7f2f8375f473bcbd91c59516a898
sinbadBahri <h.l.kh7990@gmail.com>
2023-11-09 13:39:08 +0330

    End to endpoint test: Testing API client Product Slug

--- shop/config/tests/product/test_endpoints.py
+++ shop/config/tests/product/test_endpoints.py
@@ -44,3 +44,12 @@ class TestProductEndpoints:
         # Assert
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 4
+
+    def test_return_single_product_by_slug(self, product_factory, api_client):
+        obj = product_factory(slug='test-slug')
+        response = api_client().get(f"{self.endpoint}{obj.slug}/")
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 1
+
+    def test_return_products_by_category_name(self):
+        pass
