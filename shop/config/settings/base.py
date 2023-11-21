8e2bbc7ea706b70619b1aa18721aca24021a0934
sinbadBahri <h.l.kh7990@gmail.com>
2023-11-21 15:05:00 +0330

    login & logout added

--- shop/config/settings/base.py
+++ shop/config/settings/base.py
@@ -101,6 +101,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
--- shop/config/urls.py
+++ shop/config/urls.py
@@ -13,6 +13,7 @@ router.register(r"products", views.ProductViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
