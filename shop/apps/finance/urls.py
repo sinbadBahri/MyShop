e6b566d4a1c794b58601eb128f36eb2211450b94
sinbadBahri <h.l.kh7990@gmail.com>
2023-12-12 12:05:17 +0330

    Temporary Architecture finished successfully

--- shop/apps/finance/urls.py
+++ shop/apps/finance/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ChargeWalletView, VerifyView, PaymentView, PaymentGatewayView
+from .views import VerifyView, PaymentView, PaymentAPIView, PaymentGatewayView
 
 # temp
 urlpatterns = [
-    path('charge/', ChargeWalletView.as_view(), name="charge"),
+    # path('charge/', ChargeWalletView.as_view(), name="charge"),
     path('verify/', VerifyView.as_view(),  name="verify"),
     path('pay/<str:invoice_number>/', PaymentView.as_view(), name="payment"),
-    path('pay/<str:invoice_number>/<str:gateway_code>/', PaymentGatewayView.as_view(), name="payment-gateway")
+    path('pay/<str:invoice_number>/<str:gateway_code>/', PaymentGatewayView.as_view(), name="payment-gateway"),
 ]
--- shop/apps/finance/views.py
+++ shop/apps/finance/views.py
@@ -3,44 +3,47 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django.views import View
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .forms import ChargeWalletForm
 from .models import Payment, Gateway
+
 from .utils.zarinpal import zarrinpal_request_handler, zarrinpal_payment_checker
 
-
-class ChargeWalletView(View):
-    template_name = 'charge_wallet.html'
-    form_class = ChargeWalletForm
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'form': self.form_class})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        MMERCHANT_ID = settings.ZARRINPAL['merchant_id']
-        description = ""
-        email = 'aqaSaeed@userurl.ir'  # Optional
-        mobile = '09115006072'  # Optional
-        CallbackURL = settings.ZARRINPAL['gateway_callback_url']
-
-        # 3
-        if form.is_valid():
-            amount = form.cleaned_data['amount']
-            payment_link, authority = zarrinpal_request_handler(
-                        MMERCHANT_ID,
-                        amount,
-                        description,
-                        email,
-                        mobile,
-                        CallbackURL
-                    )
-
-            if payment_link is not None:
-                print("\n\n*************\n", authority)
-                print("\n\n***********\n", payment_link)
-                return redirect(payment_link)
-
-        return render(request, self.template_name, {'form': form})
+# Test View
+# class ChargeWalletView(View):
+#     template_name = 'charge_wallet.html'
+#     form_class = ChargeWalletForm
+#
+#     def get(self, request, *args, **kwargs):
+#         return render(request, self.template_name, {'form': self.form_class})
+#
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#         MMERCHANT_ID = settings.ZARRINPAL['merchant_id']
+#         description = ""
+#         email = 'aqaSaeed@userurl.ir'  # Optional
+#         mobile = '09115006072'  # Optional
+#         CallbackURL = settings.ZARRINPAL['gateway_callback_url']
+#
+#         if form.is_valid():
+#             amount = form.cleaned_data['amount']
+#             payment_link, authority = zarrinpal_request_handler(
+#                         MMERCHANT_ID,
+#                         amount,
+#                         description,
+#                         email,
+#                         mobile,
+#                         CallbackURL
+#                     )
+#
+#             if payment_link is not None:
+#                 print("\n\n*************\n", authority)
+#                 print("\n\n***********\n", payment_link)
+#                 return redirect(payment_link)
+#
+#         return render(request, self.template_name, {'form': form})
 
 
 class VerifyView(View):
@@ -64,6 +67,7 @@ class VerifyView(View):
         return render(request, self.template_name, {'payment': payment})
 
 
+# ********************************          Test 1               *********************************
 class PaymentView(View):
     template_name = 'payment.html'
 
@@ -76,6 +80,22 @@ class PaymentView(View):
         gateways = Gateway.objects.filter(is_enable=True)
 
         return render(request, self.template_name, {'payment': payment, 'gateways': gateways})
+
+
+# ********************************          Test 2               *********************************
+class PaymentAPIView(APIView):
+    def get(self, request, invoice_number, *args, **kwargs):
+        try:
+            payment = Payment.objects.get(invoice_number=invoice_number)
+        except Payment.DoesNotExist:
+            raise Http404
+
+        gateways = Gateway.objects.filter(is_enable=True)
+
+        payment_serializer = PaymentSerializer(payment)
+        gateway_serializer = GatewaySerializer(gateways, many=True)
+
+        return Response({'payment': payment_serializer.data, 'gateways': gateway_serializer.data})
 
 
 class PaymentGatewayView(View):
